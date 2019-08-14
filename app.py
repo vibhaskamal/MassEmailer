2570edcb488cbbc661258e44b005b56486ec481f
Vibhas <vkam0002@student.monash.edu>
2019-08-14 23:27:01 +1000

    Refactored code to make the reading functionlity a function

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import xlrd
 
 file_name= 'Details.xlsx'
-workbook = xlrd.open_workbook(file_name)
-worksheet = workbook.sheet_by_name("Sheet1")
+sheet_name = "Sheet1"
 
-num_rows = worksheet.nrows
-num_cols = worksheet.ncols
+def read_data(file_name, sheet):
+    workbook = xlrd.open_workbook(file_name)
+    worksheet = workbook.sheet_by_name(sheet)
 
-file_data =[]
-for row in range(0, num_rows):
-    row_data = []
-    for col in range(0, num_cols):
-        data = worksheet.cell_value(row, col)
-        row_data.append(data)
-    file_data.append(row_data)
+    num_rows = worksheet.nrows
+    num_cols = worksheet.ncols
 
+    file_data =[]
+    for row in range(0, num_rows):
+        row_data = []
+        for col in range(0, num_cols):
+            data = worksheet.cell_value(row, col)
+            row_data.append(data)
+        file_data.append(row_data)
+    
+    return file_data
+
+
+file_data = read_data(file_name, sheet_name)
 print(file_data)
 print(file_data[1][1])
