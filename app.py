a5d743f28ed7e6764a14f85e4a3837c7ba5bbae2
Vibhas <vkam0002@student.monash.edu>
2019-08-15 11:12:30 +1000

    Code cleanup

--- app.py
+++ app.py
@@ -30,80 +30,22 @@ file_data = read_data(file_name, sheet_name)
 # print(file_data)
 # print(file_data[1][1])
 
-# msd = email.MIMEMultipart()
 
-
-# def sendMail(sender_email, receiver_email, password, smtp_server="localhost"):
 def sendMail():
-    # port = 1025  # For SSL
-    # message = """\
-    # Subject: Test message\
-    
-    # This is a test message."""
-
-    # msg = email.MIMEMultipart()
-    # msg['From'] = sender_email
-    # msg['To'] = receiver_email
-    # msg['Subject'] = "Python email"
-
-    # body = "Python test mail"
-    # msg.attach(email.MIMEText(body, 'plain'))
-
-    # text = msg.as_string()
-
-    # text = """From: Adam
-    # To: Sandler
-    # Subject: Python email
-    
-    # Python email app.
-    # """    
-    # # context = ssl.create_default_context()
-    # # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    # #     server.login(sender_email, password)
-    # #     server.sendmail(sender_email, receiver_email, text)
-
-    # with smtplib.SMTP(smtp_server, port) as server:
-    #     # server.ehlo()  # Can be omitted
-    #     # server.starttls(context=context)
-    #     # server.ehlo()  # Can be omitted
-    #     server.login(sender_email, password)
-    #     server.sendmail(sender_email, receiver_email, text)
-
-    # port = 587
-    # smtp_server = "smtp.gmail.com"
-    # # sender_email = ""
-    # # receiver_email = ""
-    # # password = ""
-    # message = """\
-    # Subject: Testing\n
-
-    # Testing the proper functioning of this app."""
-    
-    # context = ssl.create_default_context()
-    # with smtplib.SMTP(smtp_server, port) as server:
-    #     server.ehlo()
-    #     server.starttls(context=context)
-    #     server.ehlo()
-    #     server.login(sender_email, password)
-    #     server.sendmail(sender_email, receiver_email, message)
-    # 
-    # set up the SMTP server
+    # Setting up the SMTP server details
     s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+
+    # Start the TLS session
     s.starttls()
-    s.login("email", "password")      
 
-    msg = MIMEMultipart()       # create a message
+    s.login("", "")      
 
-    # add in the actual person name to the message template
-    # message = message_template.substitute(PERSON_NAME=name.title())
-
-    # Prints out the message body for our sake
-    # print(message)
+    msg = MIMEMultipart()
 
     # setup the parameters of the message
     msg['From']=""
     msg['To']=""
-    msg['Subject']="Python app"
+    msg['Subject']="Python app Part 2"
     
     message = "Hello, how are you?"
 
@@ -117,7 +59,9 @@ def sendMail():
     
     # Terminate the SMTP session and close the connection
     s.quit()
+
     print("Done")
+
 
 sendMail()
 
