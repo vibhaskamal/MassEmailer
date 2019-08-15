46d40458da7f3f0085deb259259416d62ad958e9
Vibhas <vkam0002@student.monash.edu>
2019-08-15 11:06:25 +1000

    Modified code to be able to set from, to and subject in the email

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import xlrd
 import smtplib
 import ssl
+import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 file_name= 'Details.xlsx'
 sheet_name = "Sheet1"
@@ -27,20 +30,95 @@ file_data = read_data(file_name, sheet_name)
 # print(file_data)
 # print(file_data[1][1])
 
-port = 587
-smtp_server = "smtp.gmail.com"
-sender_email = ""
-receiver_email = ""
-password = ""
-message = """\
-Subject: Testing
+# msd = email.MIMEMultipart()
 
-Testing the proper functioning of this app."""
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()
-    server.starttls(context=context)
-    server.ehlo()
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+# def sendMail(sender_email, receiver_email, password, smtp_server="localhost"):
+def sendMail():
+    # port = 1025  # For SSL
+    # message = """\
+    # Subject: Test message\
+    
+    # This is a test message."""
+
+    # msg = email.MIMEMultipart()
+    # msg['From'] = sender_email
+    # msg['To'] = receiver_email
+    # msg['Subject'] = "Python email"
+
+    # body = "Python test mail"
+    # msg.attach(email.MIMEText(body, 'plain'))
+
+    # text = msg.as_string()
+
+    # text = """From: Adam
+    # To: Sandler
+    # Subject: Python email
+    
+    # Python email app.
+    # """    
+    # # context = ssl.create_default_context()
+    # # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    # #     server.login(sender_email, password)
+    # #     server.sendmail(sender_email, receiver_email, text)
+
+    # with smtplib.SMTP(smtp_server, port) as server:
+    #     # server.ehlo()  # Can be omitted
+    #     # server.starttls(context=context)
+    #     # server.ehlo()  # Can be omitted
+    #     server.login(sender_email, password)
+    #     server.sendmail(sender_email, receiver_email, text)
+
+    # port = 587
+    # smtp_server = "smtp.gmail.com"
+    # # sender_email = ""
+    # # receiver_email = ""
+    # # password = ""
+    # message = """\
+    # Subject: Testing\n
+
+    # Testing the proper functioning of this app."""
+    
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP(smtp_server, port) as server:
+    #     server.ehlo()
+    #     server.starttls(context=context)
+    #     server.ehlo()
+    #     server.login(sender_email, password)
+    #     server.sendmail(sender_email, receiver_email, message)
+    # 
+    # set up the SMTP server
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.starttls()
+    s.login("email", "password")      
+
+    msg = MIMEMultipart()       # create a message
+
+    # add in the actual person name to the message template
+    # message = message_template.substitute(PERSON_NAME=name.title())
+
+    # Prints out the message body for our sake
+    # print(message)
+
+    # setup the parameters of the message
+    msg['From']=""
+    msg['To']=""
+    msg['Subject']="Python app"
+    
+    message = "Hello, how are you?"
+
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+    
+    # send the message via the server set up earlier.
+    s.send_message(msg)
+
+    del msg
+    
+    # Terminate the SMTP session and close the connection
+    s.quit()
+    print("Done")
+
+sendMail()
+
+print("Successful")
